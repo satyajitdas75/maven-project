2023809d62d8c9df9e67fd28e9769e77a8043771
Satyajit Das (Financial Services) <SA345476@wipro.com>
2017-08-24 17:09:16 +0530

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
 /**
-* @param String someone name of a person
-* @return string greetings
+* @param someone String someone name of a person
+* @return greetings string greetings
 */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
